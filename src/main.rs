a2457ef0fb9512fea1baf519cae190e1fb1bd8ed
danimal141 <hideaki.ishii1204@gmail.com>
2021-01-11 15:35:33 +0900

    Try to use whoami crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::io;
 
 use monkey::repl;
+use whoami::username;
 
 fn main() {
-    println!("Hello! This is the Monkey programming language!");
+    println!(
+        "Hello {:?}! This is the Monkey programming language!",
+        username()
+    );
     println!("Feel free to type in commands");
 
     repl::start(io::stdin(), io::stdout());
