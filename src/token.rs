54d03aa0b7c20f1644b76e820a2eb5359b935713
danimal141 <hideaki.ishii1204@gmail.com>
2021-01-11 15:13:28 +0900

    Refactor

--- src/token.rs
+++ src/token.rs
@@ -1,11 +1,11 @@
-#[derive(Debug, PartialEq)]
-pub enum TokenType {
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Token {
     Illegal,
     Eof,
 
     // Identifiers and literals
-    Ident, // add, foobar, x, y, ...
-    Int,   // 12345
+    Ident(String), // add, foobar, x, y, ...
+    Int(i32),      // 12345
 
     // Operators
     Assign,
@@ -37,40 +37,17 @@ pub enum TokenType {
     Return,
 }
 
-#[derive(Debug, PartialEq)]
-pub struct Token {
-    pub token_type: TokenType,
-    pub literal: String,
-}
-
 impl Token {
-    pub fn new(token_type: TokenType, ch: char) -> Token {
-        let mut str = String::new();
-        str.push(ch);
-        Token {
-            token_type,
-            literal: str,
+    pub fn lookup_token_type(ident: &str) -> Token {
+        match ident {
+            "let" => Token::Let,
+            "fn" => Token::Function,
+            "true" => Token::True,
+            "false" => Token::False,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "return" => Token::Return,
+            _ => Token::Ident(ident.to_string()),
         }
-    }
-
-    pub fn from_identifier(ident: String) -> Token {
-        let token_type = lookup_token_type(&ident);
-        Token {
-            token_type,
-            literal: ident,
-        }
-    }
-}
-
-fn lookup_token_type(ident: &str) -> TokenType {
-    match ident {
-        "let" => TokenType::Let,
-        "fn" => TokenType::Function,
-        "true" => TokenType::True,
-        "false" => TokenType::False,
-        "if" => TokenType::If,
-        "else" => TokenType::Else,
-        "return" => TokenType::Return,
-        _ => TokenType::Ident,
     }
 }
